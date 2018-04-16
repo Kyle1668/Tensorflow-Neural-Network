84831a87e38f46e1bd2e74cc3d3d6010832eba3f
Kyle1668 <kyledevinobrien1@gmail.com>
2018-04-16 08:54:12 -0700

    prog

--- network.py
+++ network.py
@@ -1,16 +1,28 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
-n_features = 5
-n_dense_neurons = 3
 
-input_data = tf.placeholder(dtype=tf.float32, shape=[None, n_features])
-weights = tf.Variable(tf.random_normal([n_features, n_dense_neurons]))
-bias = tf.Variable(tf.ones([n_dense_neurons]))
-output = tf.add(tf.matmul(input_data, weights), bias)
-activation = tf.sigmoid(output)
+def network_layer():
+    n_features = 5
+    n_dense_neurons = 3
 
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-    layer_out = sess.run(activation, feed_dict={input_data: np.random.random([1, n_features])})
-    print(layer_out)
+    input_data = tf.placeholder(dtype=tf.float32, shape=[None, n_features])
+    weights = tf.Variable(tf.random_normal([n_features, n_dense_neurons]))
+    bias = tf.Variable(tf.ones([n_dense_neurons]))
+    output = tf.add(tf.matmul(input_data, weights), bias)
+    activation = tf.sigmoid(output)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        layer_out = sess.run(activation, feed_dict={input_data: np.random.random([1, n_features])})
+        print(layer_out)
+
+
+def regression_example():
+    x_data = np.linspace(0, 1, 10) + np.random.uniform(-1.5, 1.5, 10)
+    y_data = np.linspace(0, 10, 10) + np.random.uniform(-1.5, 1.5, 10)
+    plt.plot(x_data, y_data)
+
+
+regression_example()
