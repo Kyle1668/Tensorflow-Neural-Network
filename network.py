8d5d4fbfacdc162ab1701f9099d2a0f0aca38d97
Kyle1668 <kyledevinobrien1@gmail.com>
2018-04-16 08:18:53 -0700

    refactored code

--- network.py
+++ network.py
@@ -4,16 +4,13 @@ import tensorflow as tf
 n_features = 5
 n_dense_neurons = 3
 
-x = tf.placeholder(dtype=tf.float32, shape=(None, n_features))
-w = tf.Variable(tf.random_normal([n_features, n_dense_neurons]))
-bias_term = tf.Variable(tf.ones([n_dense_neurons]))
-
-# Post Operations
-xw = tf.matmul(x, w)
-z = tf.add(xw, bias_term)
-activation = tf.nn.sigmoid(z)
+input_data = tf.placeholder(dtype=tf.float32, shape=[None, n_features])
+weights = tf.Variable(tf.random_normal([n_features, n_dense_neurons]))
+bias = tf.Variable(tf.ones([n_dense_neurons]))
+output = tf.add(tf.matmul(input_data, weights), bias)
+activation = tf.sigmoid(output)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    layer_out = sess.run(activation, feed_dict={x: np.random.random([1, n_features])})
+    layer_out = sess.run(activation, feed_dict={input_data: np.random.random([1, n_features])})
     print(layer_out)
