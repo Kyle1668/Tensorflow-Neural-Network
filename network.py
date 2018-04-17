80624cdbe821e2167a5c313e310f429cc13f0e3e
Kyle1668 <kyledevinobrien1@gmail.com>
2018-04-16 20:52:43 -0700

    prog

--- network.py
+++ network.py
@@ -38,5 +38,49 @@ def regression_example(starting_point, stopping_point, num_points):
     plt.show()
 
 
+def nn_example(starting_point, stopping_point, num_points):
+    """[Function to solve a linear quadratin equation.]
+    
+    Arguments:
+        starting_point {[int]} -- [The lower bound of the random numbers.]
+        stopping_point {[int]} -- [The upper bound of the random numbers.]
+        num_points {[int]} -- [The number of points between the lower an dupper bound to be generated.]
+    """
+
+    x_data = np.linspace(starting_point, stopping_point, num_points) + np.random.uniform(-1.5, 1.5, 10)
+    y_data = np.linspace(starting_point, stopping_point, num_points) + np.random.uniform(-1.5, 1.5, 10)
+
+    rands = np.random.rand(2)
+    m = tf.Variable(rands[0])
+    b = tf.Variable(rands[1])
+    error = 0
+
+    for x, y in zip(x_data, y_data):
+        y_hat = m*x + b    
+        error += (y - y_hat)**2
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train = optimizer.minimize(error)
+        
+    init = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        sess.run(init)
+        training_steps = 50000
+
+        for i in range(training_steps):
+            print(sess.run(error))
+            sess.run(train)
+            
+        final_slope, final_inetcept = sess.run([m, b])
+        x_test = np.linspace(-1, 11, 10)
+        y_pred_plot = final_slope * x_test + final_inetcept
+
+
+        plt.plot(x_test, y_pred_plot)
+        plt.plot(x_data, y_data)
+        plt.show()
+
+
+
 # network_layer(5, 3)
-regression_example(0, 1, 10)
+nn_example(0, 10, 10)
